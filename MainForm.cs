9dac6ec3e5282e118a98204a44e94b3dcdc3c7b6
Adam Mikušovský <a.mikusovsky@gmail.com>
2016-07-26 22:16:06 +0200

    Code refactoring - remove unused references

--- MainForm.cs
+++ MainForm.cs
@@ -1,15 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Windows.Forms;
 using Emgu.CV;
 using Emgu.CV.Structure;
-using Emgu.Util;
-using System.Threading;
 using Emgu.CV.CvEnum;
 using Emgu.CV.VideoSurveillance;
 using System.Diagnostics;
